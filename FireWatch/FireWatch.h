cc96e822b604e0afa506e0b2992607c2b74f7db5
Meniny <meniny@qq.com>
2017-04-20 16:43:58 +0800

    1.1.2

---
+++ FireWatch/FireWatch.h
@@ -0,0 +1,19 @@
+//
+//  FireWatch.h
+//  FireWatch
+//
+//  Created by Meniny on 2017-04-20.
+//  Copyright © 2017年 Meniny. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for FireWatch.
+FOUNDATION_EXPORT double FireWatchVersionNumber;
+
+//! Project version string for FireWatch.
+FOUNDATION_EXPORT const unsigned char FireWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireWatch/PublicHeader.h>
+
+
